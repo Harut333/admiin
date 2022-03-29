3396c4db30234224a155afc341a5aaca666a070a
Harut333 <harutavetisyan0@gmail.com>
2022-03-30 01:06:17 +0400

    admiin

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ for (let expand of expands) {
     expand.onclick = function () {
         posts.forEach((item) => {
             item.classList.remove("less")
+            item.nextElementSibling.style.width = "88%"
+            // item.nextElementSibling.style.paddingRight = "220px"
             item.nextElementSibling.textContent = "Expand"
+            
+            
+
         })
         this.parentElement.querySelector(".textBox").classList.add("less")
         this.textContent = "Collapse"
